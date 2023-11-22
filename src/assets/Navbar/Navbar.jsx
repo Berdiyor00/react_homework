6da47364c6c56fe7a319823b463ac981d0aa9461
Berdiyor00 <berdiyorhasanov1@gmail.com>
2023-11-22 14:25:40 +0500

    Modalka

--- src/assets/Navbar/Navbar.jsx
+++ src/assets/Navbar/Navbar.jsx
@@ -4,9 +4,14 @@ import Bar from '../img/2292434-200.png';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [openCard, setOpenCard] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const Open = () => {
+    setOpenCard(!openCard);
   };
 
   return (
@@ -32,14 +37,36 @@ const Navbar = () => {
                   <a href="#">Contact</a>
                 </li>
               </ul>
-             
             </div>
             <button onClick={toggleMenu}>
-                <img src={Bar} alt="" className="Bar__img" />
-              </button>
+              <img src={Bar} alt="" className="Bar__img" />
+            </button>
           </div>
         </nav>
       </header>
+
+      <section>
+        <div className="container">
+          <p className={openCard ? 'back' : ''}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore maxime, sint et modi eveniet totam culpa
+            quod unde eligendi, amet qui tempora voluptas veniam aliquid quisquam! Repellat, magnam, ut at inventore
+            sapiente dolores voluptatibus laudantium quod magni tempore ab commodi modi quis dolor eaque harum ea aperiam
+            est ullam consequuntur accusantium officia veritatis? Totam delectus eligendi sapiente nisi ex id ipsam aut
+            facilis distinctio modi aliquid dolorum asperiores repellat sequi non soluta voluptates, eius accusantium,
+            facere repellendus laboriosam deserunt? Dolorem veniam iure cupiditate omnis. Facilis quisquam sed ex non natus
+            blanditiis quaerat voluptatibus voluptate dolor tempora tenetur, hic ullam enim possimus similique soluta
+            quasi dicta eligendi? Modi quisquam quidem saepe quia similique ratione quaerat aliquam explicabo voluptas
+            at, eos, praesentium eius beatae totam dolorem dolor officiis tempore quam voluptatum iste illo! Vel iste
+            quo animi excepturi eveniet quibusdam explicabo amet labore velit ipsam? Neque, iusto. Labore repellat
+            numquam natus accusantium qui. Laudantium suscipit delectus dolorem ab explicabo eos sunt eum sit ipsum aut
+            a hic commodi natus, error assumenda ex ipsa debitis praesentium ad nam veniam, expedita iusto facere.
+            Repudiandae hic tempora voluptatibus velit ipsam soluta a minima voluptates iste, earum debitis quod, labore
+            rerum aperiam maxime eveniet explicabo? Repudiandae.
+          </p>
+          <button onClick={Open} className='Open__Text'> Click Me</button>
+        </div>
+
+      </section>
     </div>
   );
 };
